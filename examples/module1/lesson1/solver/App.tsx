1afb032cf979a24a70678bbf80de913d7fb0b720
Pawel <pmor80@Pawels-MacBook-Pro.local>
2024-02-21 18:03:24 +0000

    Remove redundant component

--- examples/module1/lesson1/solver/App.tsx
+++ examples/module1/lesson1/solver/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Input from './components/Input';
-import Button from './components/Buttons';
+import Button from './components/Button';
 const App = () => {
   const [numA, setNumA] = useState<number>(0);
   const [numB, setNumB] = useState<number>(0);
