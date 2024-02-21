17d0ee4bcd9caa771693862c2035ee8b0f86fe3d
pmor80 <me.pawel.morawski@gmail.com>
2024-02-21 18:25:06 +0000

    Add Input and Button Components

--- examples/module1/lesson1/solver/functions.ts
+++ examples/module1/lesson1/solver/functions.ts
@@ -11,3 +11,13 @@ export function f3(a, b) {
 export function f4(a, b) {
   return a / b;
 }
+
+export function f(a, b, operator) {
+  return operator === '+'
+    ? f1(a, b)
+    : operator === '-'
+    ? f2(a, b)
+    : operator === '*'
+    ? f3(a, b)
+    : f4(a, b);
+}
